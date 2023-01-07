17ca2ab173e9b53668341a9ff51fd0c7f89ca42c
dani_prayogi <danituil58@gmail.com>
2023-01-07 09:32:19 +0700

    init

--- ios/taskDansTests/taskDansTests.m
+++ ios/taskDansTests/taskDansTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface taskTanyoTests : XCTestCase
+@interface taskDansTests : XCTestCase
 
 @end
 
-@implementation taskTanyoTests
+@implementation taskDansTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
